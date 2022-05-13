e58e1316a70edb6bb6d8b783bcc3bc8a180ef7e7
Eduardo Bovareto <Eduardo.ebs57@gmail.com>
2022-05-12 21:41:13 -0300

    'Revalidando Apagado'

--- Exercicio-forms-1435.py
+++
@@ -1,5 +0,0 @@
-nInt = int(input('Informe um inteiro'))
-if nInt < 0 or nInt > 100:
-    print('VALOR FORA DE RANGE N(0 < N < 100)!')
-    while nInt < 0 or nInt > 100:
-        nInt = int(input('Favor Reemitir um valor inteiro'))

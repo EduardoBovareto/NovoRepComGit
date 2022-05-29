175ac4d4a44100de62c9a84434d6e43671388522
Eduardo Bovareto <Eduardo.ebs57@gmail.com>
2022-05-29 18:05:51 -0300

    Limpando código e fazendo de forma mais facil

--- Desafios/CadastroDeMateriais.py
+++ Desafios/CadastroDeMateriais.py
@@ -1,22 +1,31 @@
 # SUBPROGRAMAÇÃO
 def CadastraMateriais(Mat): # Pegará os materiais e colocará em matraiz
     Matriz = [] # Matriz final
-    temporario = [] # Materiais 
-    
-    for i in Mat: # Percorre as informações para diferenicar cada uma
-
-        if i.isnumeric(): # Caso de numero de material
-            i = int(i) # Cso seja válido, se converte o numero antes em string
-            temporario.append(i) # Adicionando o valor em forma inteira
+    temporario = [] # Materiais
+    i = 0
+    while i != len(Mat): #  i começa em 0 e é diferente de len(Mat) == 4 até que i chegue a 4
+        while len(temporario) != 2: #Temporario começa com 0 e deve ser adicionado 2 elementos 
+            if Mat[i].isnumeric():
+                temporario.append(int(Mat[i])) # Ou temporário = list(map(int, Mat[i]))
+            else:
+                temporario.append(Mat[i])
+            i += 1 # Sempre aqui dentro, pois ao terminar as verificações ele incrementa, se estiver fora não muda a posicao de Mat[i].
+        Matriz.append(temporario)
+        temporario = [] 
         
-        else:
-            temporario.append(i) # Caso onde é o nome do material
+    
+    # for i in Mat: # Percorre as informações para diferenicar cada uma
+    #     if i.isnumeric(): # Caso de numero de material
+    #         i = int(i) # Cso seja válido, se converte o numero antes em string
+    #         temporario.append(i) # Adicionando o valor em forma inteira
+    #     else:
+    #         temporario.append(i) # Caso onde é o nome do material
 
-        if len(temporario) == 2: # Condição de array temporario saturar cada material
-            Matriz.append(temporario) # Adiciona as informações a matriz
-            temporario = [] # limpa o aray temporário
-            continue # Passa para o próximo i
-    # Percorrer 2 laços para gerar a matriz e adicionar as informações:
+    #     if len(temporario) == 2: # Condição de array temporario saturar cada material
+    #         Matriz.append(temporario) # Adiciona as informações a matriz
+    #         temporario = [] # limpa o aray temporário
+    #         continue # Passa para o próximo i
+    
     j = 0
     for i in range(len(Matriz)): # Laço que percorre cada linha da matriz
         for j in Matriz[i]: # laço que percorre cada coluna da matriz
@@ -29,5 +38,3 @@ def CadastraMateriais(Mat): # Pegará os materiais e colocará em matraiz
 materiais = input('Informe os materiais, a quantidade').split() # Coleta os materias da loja
 CadastraMateriais(materiais) # Chamada da function
 # Podemos fazer contas com o aumento e preço de produto.
-
-#list(int(materiais.split())) or list(map(int, Array.split())) generation of the a list random or with values selctioned

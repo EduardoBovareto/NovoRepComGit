665941da5d9e46e310737e89536595db7ef03dae
Eduardo Bovareto <Eduardo.ebs57@gmail.com>
2022-06-23 17:41:03 -0300

    Validando codigo e proxima quest

--- APS/APX3/Q1-APX3-2022.py
+++ APS/APX3/Q1-APX3-2022.py
@@ -1,23 +1,19 @@
 #SUBPROGRAMA
 from math import sqrt
 def AchaDiferenca(X, nm):
-    D1 = 0 #Distância de pontos X1 e X2
+    D1 = Distancia = 0 #Distância de pontos X1 e X2
     D2 = 0 #Distância de pontos Y1 e Y2
     i = 0
-    Dist = -1
-    with open(nm, 'r') as Linhas:
-        for Linha in Linhas:
-            Y = list(map(int, Linha.split()))  
-            D1 = (X[i] - X[i + 1]) ** 2 #x1, x2
-            D2 = (Y[i] - Y[i + 1]) ** 2 #y1, y2
-            Distancia = sqrt(D1 + D2)
-            if Distancia > Dist:
-                Dist = Distancia
-    return Dist
-
+    for Linha in nm:
+        Y = list(map(int, Linha.split()))  
+        D1 = (X[i] - X[i + 1]) ** 2 #x1, x2
+        D2 = (Y[i] - Y[i + 1]) ** 2 #y1, y2
+        Distancia += sqrt(D1 + D2)
+    return Distancia
 def ProcessaPontos(Arquivo):
+    Diferenca = 0
     Arquivo  = open(Arquivo, 'r')
-    somaPontos = i = 0
+    i = 0
     if len(Arquivo.read()) == 0 :
            print('Arquivo de pontos está vazio!!!')
            return None
@@ -26,12 +22,12 @@ def ProcessaPontos(Arquivo):
         for Linha in Arquivo:
             # Linha = Arquivo.readline()
             print(Linha)#Formalizar
-    for Linha in Arquivo:
-        Pontos = list(map(int, Linha.split()))
-        Diferenca += AchaDiferenca(Pontos, Arquivo)
-    print()
+            Pontos = list(map(int, Linha.split()))
+            Diferenca += AchaDiferenca(Pontos, Arquivo)
+    print(Diferenca)
     Arquivo.close()
     return None
 #PROGRAMA PRINCIPAL
 Nome = input('Informe o nome do arquivo: ')
 ProcessaPontos(Nome)
+#Vazio.txt; UmPonto.txt; Pontos.txt

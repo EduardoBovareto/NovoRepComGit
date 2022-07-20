32afe55cb3807cbf539a10d6f1596c98dbe1dd18
Eduardo Bovareto <Eduardo.ebs57@gmail.com>
2022-07-20 19:47:52 -0300

    Validando Codigo de criptografia terminado e fializando o dia

--- Criptografia/Criptografia2.py
+++ Criptografia/Criptografia2.py
@@ -2,22 +2,28 @@
 import math
 import random as rd
 def criptografia(Palavra, Nivel): # O nível é o quantos caracteres se pula na relação Unicode
+    temp = ''
     if Nivel == 'Baixa':
         for indice, letra in enumerate(Palavra):
-            Palavra[indice] = chr(ord(letra) + 2) # Pega a próxima posicao alfebética
+            temp+= chr(ord(letra) + 2) # Pega a próxima posicao alfebética
             #chr converte um inteiro em string com base em codificação Unicode, dentro do padrao
+        Palavra = temp
         return None, print(f'A palavra codificada é: {Palavra}')# Muda a posição de cada letra em 1 unidade em padrao unicode.
 
     elif Nivel == 'Media':
         for indice, letra in enumerate(Palavra):
-            Palavra[indice] = chr(ord(letra) * 3 + 2)
+            R = (ord(letra) * 3) + 2
+            temp += chr(R)
+        Palavra = temp
         return None, print(f'A palavra codificada é: {Palavra}') # Muda cada posição em 3 vezes adcionado a 2 em padrao unicode.
 
     elif Nivel == 'Alta':
         for indice, letra in enumerate(Palavra):
-            rq = math.sqrt(ord(letra) * 5)
-            temp = rd.randrange(0, rd.randint(-5, 200)) * 3
-            Palavra[indice] = chr(rq + temp)
+            rq = (math.sqrt(ord(letra) * 5))
+            rq = math.trunc(rq)
+            rq += rd.randrange(0, rd.randint(-5, 200)) * 3
+            temp += chr(rq)
+        Palavra = temp
         return None, print(f'A palavra codificada é: {Palavra} ')
         
 Info = input('Digite a palavra: ')

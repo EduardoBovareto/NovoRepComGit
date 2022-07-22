1a3a4aa0ffc13479981819c5a1378029cff50270
Eduardo Bovareto <Eduardo.ebs57@gmail.com>
2022-07-22 19:38:48 -0300

    Fializando dados

--- Desafios/Arquivo_BaseData/ManipulaArquivo.py
+++ Desafios/Arquivo_BaseData/ManipulaArquivo.py
@@ -6,13 +6,13 @@ def processa_Arquivo(Arq, m = None): #Irá pegar todos os valores e somar
     Arq = 'Arquivo_BaseData\\' + Arq
     if '.txt' not in Arq:
         Arq += '.txt'
-
     with open(Arq, 'r+') as Linhas:
         total = 0 
         for linha in Linhas:
-            valor = linha.split()#Pega cada parte da linha e guarda
-            if valor == []:
+            if linha == '\n':
                 break
+
+            valor = linha.split()#Pega cada parte da linha e guarda
             for i in range(len(valor)):
                 if '.' in valor[i] and valor[i][0].isnumeric(): #Verifica se é um valor mesmo
                     valor[i] = float(valor[i])
@@ -25,7 +25,6 @@ def processa_Arquivo(Arq, m = None): #Irá pegar todos os valores e somar
         '''
         Formatacao de data, não precisa chamar denovod dt.datetime... pois o tipo ja  é datetime, o que resta é só conversao
         '''
-
         Linhas.write(f'Data de Registro: {Rdate}')
         print('The total amount processed was: R${}'.format(total))
         total = 0
@@ -33,30 +32,30 @@ def processa_Arquivo(Arq, m = None): #Irá pegar todos os valores e somar
     '''Duas coisas a tratar: 1 é verficar e colocar .txt no nome do arquivo para caso de
         erro, outra é retirar a repetiçao de escrita do valor total!'''
 
-def acha_nome():
-    pass
+def acha_nome(name, Arq):
+    valor_vendido = 0
+    Arq = 'Arquivo_BaseData\\' + Arq
+    if '.txt' not in Arq:
+        Arq += '.txt'
+    with open(Arq, 'r+') as Linhas:
+        total = 0 
+        for linha in Linhas:
+            if linha == '\n':
+                break
 
+            valor = linha.split()#Pega cada parte da linha e guarda
+            for infos in range(len(valor)):
+                if name == valor[infos]:
+                    temp = float(valor[6])
+                    valor_vendido += temp
+
+        Linhas.write(f'\nValor total vendido pelo {name} é: {str(valor_vendido)}\n')
+        data_atualizada = dt.datetime.now()
+        data_atualizada = data_atualizada.strftime('%d/%m/%y %H:%M') 
+        Linhas.write(f'Data de Registro: {data_atualizada}')
+        print('The total amount processed was: R${}'.format(valor_vendido))
+    return None
 #PROGRAMA PRINCIPAL
 name_arq = input('Write the name of the file: ')
-processa_Arquivo(name_arq)
-''' 
-Codigo abaixo se trata de uma funcao de arquivo s que dependendo da operacao se chama uma outra nova funcao definida dentro da propria funcao:
-def arquivos():
-    def chama_valor()
-    def acha()
-Sendo assim, cada uma seria chamada de acordo com a operacao
-
-# operation = int(input('Write the operation that want calculate: '))
-# models = [1, 2,] #Achar alguem e informar as vendas dele
-# counter = 0 
-# while operation not in models:
-#     print('Please! write a need information for processing: ')
-#     operation = int(input('Report your operation: '))
-#     counter += 0
-#     if counter == 5:
-#         print('We inform that the was program close ')
-#         break
-# else:
-#     pass
-#     # processa_arquivo(name_arq, operation)
-'''
+# processa_Arquivo(name_arq)
+acha_nome('Pablo', name_arq)

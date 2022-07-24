ee6cb48243878f0aeb6624ff4d10ef03a02d5899
Eduardo Bovareto <Eduardo.ebs57@gmail.com>
2022-07-24 09:23:16 -0300

    Salvamdo progresso e esperando futua finalizacao de codigo para achar nomes e posicoes e mudar valores de vendas

--- Desafios/Arquivo_BaseData/ManipulaArquivo.py
+++ Desafios/Arquivo_BaseData/ManipulaArquivo.py
@@ -2,6 +2,12 @@ import datetime as dt
 # import ManipulaArquivo as mp
 ''' Manipula arquivo de texto de clientes e gera data de registro'''
 #SUBPROGRAMA - > Arquivo_BaseData\Base.txt
+def manipula_arq(Arq):
+    Arq =  'Arquivo_BaseData\\' + Arq
+    if '.txt' not in Arq:
+        Arq += '.txt'
+    return True, Arq
+
 def processa_Arquivo(Arq, m = None): #Irá pegar todos os valores e somar
     Arq = 'Arquivo_BaseData\\' + Arq
     if '.txt' not in Arq:
@@ -32,7 +38,7 @@ def processa_Arquivo(Arq, m = None): #Irá pegar todos os valores e somar
     '''Duas coisas a tratar: 1 é verficar e colocar .txt no nome do arquivo para caso de
         erro, outra é retirar a repetiçao de escrita do valor total!'''
 
-def acha_nome(name, Arq):
+def acha_nome_soma_valores(name, Arq):
     valor_vendido = 0
     Arq = 'Arquivo_BaseData\\' + Arq
     if '.txt' not in Arq:
@@ -53,26 +59,36 @@ def acha_nome(name, Arq):
         data_atualizada = data_atualizada.strftime('%d/%m/%y %H:%M') 
         Linhas.write(f'Data de Registro: {data_atualizada}')
         print('The total amount processed was: R${}'.format(valor_vendido))
-    return None
+    return name
 
-def value_edit(Arq, value):
+def acha_nome(Arq, name):
+    with open(Arq, 'r') as L:
+        for i in range(len(L)):
+            info = L.readlines(i)
+            pass
+    return name, #position
+
+def value_edit(Arq, value, name):
     ''' Muda o valor da venda feita'''
-    Arq = 'Arquivo_BaseData\\' + Arq
-    if '.txt' not in Arq:
-        Arq += '.txt'
+    _, Arq = manipula_arq(Arq)
+    acha_nome(Arq, name)
+
     with open(Arq, 'r+') as valores:
+        # name = acha_nome(Arq, name)
         for v in valores:
             lista = v.split()
             i = 0
-            while i < len(lista) - 1:
+            while i < (len(lista) - 1):
                 if lista[i] == value:
                     lista[i] = str(value)
                     break
-                i += 0
+                i += 1
+                #finalizacao de codigo para achar nomes e posicoes e mudar valores de vendas
 #Criptografia de código de vendedor= importar funcao pronta por mim
 #PROGRAMA PRINCIPAL
 name_arq = input('Write the name of the file: ')
 # processa_Arquivo(name_arq)
-# acha_nome('Pablo', name_arq)
+# acha_nome_soma_valores('Pablo', name_arq)
 number = int(input('Informe o dado para mudança: '))
-value_edit(name_arq, number) #Depois colocar mais valores
+nome_reference = input('Digite o dono da venda: ')
+value_edit(name_arq, number, nome_reference) #Depois colocar mais valores

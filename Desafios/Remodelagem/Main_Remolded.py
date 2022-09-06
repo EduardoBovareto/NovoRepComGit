5d0e2b5425efb1c2109d2a8d12b5fc05d57113a0
Eduardo Bovareto <Eduardo.ebs57@gmail.com>
2022-09-06 19:25:27 -0300

    Obtendo classes

--- Desafios/Remodelagem/Main_Remolded.py
+++ Desafios/Remodelagem/Main_Remolded.py
@@ -1,6 +1,42 @@
+from random import randint, choice, shuffle
 class Cliente():
-    def __init__(self, name) -> True:
+    def __init__(self, name:str) -> True: #essa seta co True significa o retorno da função, como ele irá retornar no final
        self.name = f'{name}'
-       self.Register 
-    pass
+       self.Register = list(range(randint(0, 7), 9))
+       self.unity = list(range(randint(1, 5), randint(6, 9)))
+       self.country = 'BR'
+       self.balanceInit = randint(354, 853)
     
+    #muda as configurações do cliente
+    def change_Config(self, config, data, leed) -> None:
+        if config not in Cliente.getmembers():
+            return print('Your config dont exist in we Data Base! Please request a new configuration!')
+        else:
+            Cliente.config = data
+            return print(f'Your configuration is {leed.config}, she was changed')
+
+    def show_config(self) -> True:
+        for conf in Cliente:
+            print(self.conf, '\n')
+        
+    def show_balance(self): #mostra saldo
+        print(f'Your balance is {self.balanceInit}')
+        return self.balanceInit
+
+    def change_balance(self, value) -> True:
+        pass
+
+    def loan_get(self): #pegar empréstimo
+        pass
+
+    def show_porcent(self):
+        pass
+
+    def _buy(self):
+        pass
+
+    def _sale(self):
+        pass
+    
+    def counter_sales(self):
+        pass
---
+++ Desafios/Remodelagem/Page_introduction.py
@@ -0,0 +1,4 @@
+from unicodedata import name
+from Main_Remolded import Cliente
+Eduardo = Cliente('Eduardo')
+Eduardo.show_balance()

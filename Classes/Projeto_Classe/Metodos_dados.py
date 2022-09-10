292331c7a105af7e9f60aa5405961c1be5fa264d
Eduardo Bovareto <Eduardo.ebs57@gmail.com>
2022-09-10 18:42:17 -0300

    Validando projeto de produtos de armazem

--- Classes/Projeto_Classe/Metodos_dados.py
+++ Classes/Projeto_Classe/Metodos_dados.py
@@ -1,18 +1,25 @@
 from datetime import datetime
-
 class Met:
     def __init__(self) -> None:
         pass
 
-    def fsi():
+    def fsi(): 
+#normalmente, os arquivo para retirar as constantes, devemos buscar um arquivo em pasta que tenha o nome do produto e só selecione, tipo: Molikote_fsi.txt ou Molikote_propety.txt.
 	#function of the planner security
-        with open('fispq.txt', 'r') as arq:
+        with open('Classes\\Projeto_Classe\\fispq.txt', 'r+') as arq:
             time = datetime.today()
-            time = time.strftime(' %d/%m/%Y %H:%M')
-            arq.write(time)
-            return arq
-    
+            time = time.strftime(' %d/%m/%Y %H:%M').split()
+            company = arq.readline()
+
+            if time[0] in arq.readlines():
+                return arq, company
+            else:
+                arq.write(f'\n{time[0]}')
+
+
+            return arq, company
+
     def return_l(): 
         #retorna tupla de propriedades do produto.
-        with open('propety.txt', 'r' ) as a:
+        with open('Classes\\Projeto_Classe\\Propety.txt', 'r' ) as a:
             return a.readlines()
--- Classes/Projeto_Classe/project_quimic.py
+++ Classes/Projeto_Classe/project_quimic.py
@@ -6,12 +6,14 @@ class arm:
 		def __init__(self, name):
 			self.name = name
 			self.quant = 30
-			self.security = ficha_seguranca.fsi()
-			self._propety = ficha_seguranca.return_l('Propety.txt')
-			self.company = self._propety[0] #tem de ser a primeira coisa a ser vista
+			self.security, self.company = ficha_seguranca.fsi()
+			self._propety = ficha_seguranca.return_l()
 
 		def _changeFsi():	
 			pass
+
+	# class manual:
+	# 	pass	
 
 	def conta_product()->None:
 		print(10)
@@ -20,4 +22,4 @@ Molikote = arm.prod("Molikote") #deve se passar o parametro name na definicao
 #new é uma classe do tipo arm
 #new tem.uma herança de iniciacao de classes
 #print(Molikote.name)
-
+print(Molikote.security)

3062f20d7a7fa99d8ec3bd93d9d71012448bed88
Eduardo Bovareto <Eduardo.bovareto@outlook.com>
2024-10-31 19:49:04 -0500

    Realizando exercicios de strings

--- Strings/Criptografia.py
+++ Strings/Criptografia.py
@@ -4,21 +4,34 @@ def Cripto(mensagem):
     mensagem = mensagem.strip()
     MenTemp = ''
     i = 0
-    #1 maiusculas e minuculas devem deslocar 3 posicoes a direita - usar isupper()
-    #2 a linha deve ser invertida
-    #3 A metade da string deve andar uma posicao
-
+    #1 maiusculas e minuculas devem deslocar 3 posicoes na tabela ascii a direita - usar isupper() - OK
+    #2 a linha deve ser invertida - OK
+    #3 A metade da string deve andar uma posicao - OK
+    #Falta resolver a umta linha do enunciado do problema
     for i in range(len(mensagem)):
         if (mensagem[i].isupper() or mensagem[i].islower()):
-            temp = ord(mensagem[i]) + 3#capta o codigo dentro da tabela ASCII-ord() + 3
-            #MenTemp = mensagem[:i] #pega a mensagem antes da alteração
+            temp = ord(mensagem[i]) + 3 #capta o codigo dentro da tabela ASCII-ord() + 3
             MenTemp += chr(temp)
             temp = ''
+            continue
+
+        elif (mensagem[i].isascii()):
+            temp = ord(mensagem[i]) + 3 #capta o codigo dentro da tabela ASCII-ord() + 3
+            MenTemp += chr(temp) #acha na tabela ascii qual caracter corresponde a tal
+            temp = ''
+            continue
+
     temp = MenTemp
     MenTemp = ''
-    for i in range(len(temp)): #percorre a string atual
-        MenTemp += MenTemp[i * -1] #inversao de palavras
-    return MenTemp
+    i = len(temp) -1 #percorrer de tras pra frente
+    while i >= 0:
+        MenTemp += temp[i]
+        i -= 1
+    meio = len(MenTemp)
+    meio = meio // 2 #buscando o meio da string
+    TratadoFinal = MenTemp[meio::-1]
+
+    return TratadoFinal
             
 
 m = str(input('Escreva uma mensgem para analise: '))

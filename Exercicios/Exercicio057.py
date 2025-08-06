8a3ae1e96639068ddb99c7bc5d164d3d11ad289c
Eduardo Bovareto <Eduardo.bovareto@outlook.com>
2025-08-06 16:16:07 -0300

    Validando Exercicios Python

--- Exercicios/Exercicio057.py
+++ Exercicios/Exercicio057.py
@@ -1,5 +1,7 @@
 sexo = ['M', 'F']
-r = input('Digite um sexo: ').upper()
+r = input('Digite um sexo: ').upper().strip()[0]
+#Se o cara digitar Masculino será pego o M
+#strip() retira os espaços que não são necessários e coloca em letra maiúscula
 while r not in sexo:
 	print('Fora do esperado! Digite M ou F')
 	r = input('Escreva um sexo novamente: ').upper()
--- Exercicios/Exercicio058.py
+++ Exercicios/Exercicio058.py
@@ -5,17 +5,17 @@ r = None
 
 while r != escolhido:
 	r = int(input('Adivinhe um numero de 0 a 10: '))
-	if r > 10:
+	if r > 10 or r < 0:
 		print('\nFAVOR INFORMAR ENTRE 0 E 10\n')
+	else:
+		if r != escolhido:
+			print('Você errou! Tente novamente!')
+			counter += 1
 
-	if r != escolhido:
-		print('Você errou! Tente novamente!')
-		counter += 1
+		if counter > 3:
+			if r > escolhido:
+				print('DICA! Esta antes desse numero!')
 
-	if counter > 3:
-		if r > escolhido:
-			print('DICA! Esta antes desse numero!')
-
-		if r < escolhido:
-			print('DICA! esta depois desse numero!')
+			if r < escolhido:
+				print('DICA! esta depois desse numero!')
 print(f'Você acertou!, Total de tentativas: {counter}')

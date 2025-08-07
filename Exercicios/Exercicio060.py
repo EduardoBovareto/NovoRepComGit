6e44de3c94c6b8e5e248a0e42824373c1e1c84fa
Eduardo Bovareto <Eduardo.bovareto@outlook.com>
2025-08-07 16:24:41 -0300

    Fatorial cencertado!

--- Exercicios/Exercicio060.py
+++ Exercicios/Exercicio060.py
@@ -1,10 +1,9 @@
-n = int(input('Escreva um numero para calcular o fatorial:'))
+count = n = int(input('Escreva um numero para calcular o fatorial:'))
 fat = 1
-count = 0
 p = True
 while  p == True:
-	if n == 1:
-		p = False
+	if n == 1 or n == 0:
+		break
 	fat = n * (n - 1) * fat
 	n -= 2
-print(f'O valor de {n}! = {fat}')
+print(f'O valor de {count}! = {fat}')

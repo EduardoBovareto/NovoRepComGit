34da479b61d064c1c0ea1b3467451d6b6781e68f
Eduardo Bovareto <Eduardo.bovareto@outlook.com>
2025-08-09 16:35:22 -0300

    Validando mudancas

--- Exercicios/Exercicio062PaV2.py
+++ Exercicios/Exercicio062PaV2.py
@@ -1,22 +1,15 @@
 temp = a1 = int(input('Primeiro Termo: '))
 r = int(input('Razao da PA: '))
-count = 0
+count = 10
 cond = True
 termos = 10
 while cond == True:
-    
-    if termos == 10 and count == 0:
+    if termos == 10:
         print(f'{a1}')
-        temp += r
-        count += 1
 
-    elif termos == 0:
-        print('PAUSA!')
-    
     else:
         print(f'{temp}')
         temp += r #a partir do a1 se soma a razao em temp para imprimir
-        count += 1
 
     #se estiver no primeiro termo ele emite a1, senao os temos e PAUSA quando termos == 1
     termos -= 1
@@ -31,6 +24,8 @@ while cond == True:
             print('Favor digitar numero acima de 0!')
             termos = int(input('Quantos termos deseja mais? '))
             count += termos
-         
+
+        else:
+            count += termos         
 #Quando termo == 0 ele limita na hora o while e termina
 #count que esta emitindo o valor errado, em 2 de diferença

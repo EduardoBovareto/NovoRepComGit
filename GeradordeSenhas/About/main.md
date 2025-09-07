1c01cdc8621187232b25291bcc66c717694e7635
Eduardo Bovareto <Eduardo.bovareto@outlook.com>
2025-09-07 09:08:18 -0300

    Buscando import other branch

---
+++ GeradordeSenhas/About/main.md
@@ -0,0 +1,4 @@
+## Genarate Passwoards begginer
+Sera um programa que pegara uma senha desejada e customizara ela por padrao e ira gerar mais 5 opcoes
+
+Analisar uma forma de olhar a senha digitada sem gastar muito tempo e dificil de lidar com o codigo
---
+++ GeradordeSenhas/src/main.py
@@ -0,0 +1,21 @@
+
+def Analisa_pedido(pw):
+    if str in pw:
+        temp = []
+        try:
+            temp = list(map(int,pw)) #converte caso haja so numero
+            return temp, len(temp) #retorno para manipulacao da senha, e tamanho
+
+        except ValueError:
+            count = len(pw) #tamanho para parametro
+
+def Gera_senha(): #analisara cada caracter
+    pass
+
+password = input('Descreva a senha desejada! \n').split() #guarda senha a ser analisada
+print('Sera gerado mais senhas exemplo que podem ser adotadas')
+Analisa_pedido(password)
+
+#recebe os caracteres enviados e gera mais 6 senhas, 3 aleatorias e 3 dificeis
+# 8 a 16 caracteres
+# inicialmente vai gerar qualquer tipo escrito sem forca

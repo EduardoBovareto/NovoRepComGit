a2144d69e793b3ff77131f9ec42f7c485705ed2d
Player01 <eduardo.bovareto@outlook.com>
2025-10-31 16:25:34 -0300

    ExerciciosPython

---
+++ Exercicios/Curso_Video_NumeroPextenso.py
@@ -0,0 +1,8 @@
+'''
+numero_desejado = usuario
+tupla = com valores por extenso
+'''
+n = int(input('Digite um numero entre 0 e 20:'))
+while n > 20 or n < 0:
+    n = int(input('Digite um numero entre 0 e 20:'))
+
